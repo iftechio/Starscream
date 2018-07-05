55d75bcf241637fea4cbd3d905c3f74b9526bad6
Luo Qisheng <xunfeng@ruguoapp.com>
2018-07-05 10:18:17 +0800

    Fix data race

--- Sources/WebSocket.swift
+++ Sources/WebSocket.swift
@@ -1301,6 +1301,7 @@ open class WebSocket : NSObject, StreamDelegate, WebSocketClient, WSStreamDelega
         cleanupStream()
         mutex.unlock()
         writeQueue.cancelAllOperations()
+        writeQueue.waitUntilAllOperationsAreFinished()
     }
 
 }
